6018db16ae6a8b8ed4c20ffde2b4e7956e3a7c7c
Pooja Kamble <42534109+PoojaJKamble@users.noreply.github.com>
2021-07-25 21:41:24 +0530

    main code

---
+++ virus.py
@@ -0,0 +1,22 @@
+import turtle
+import time
+
+turtle.bgcolor('black')
+turtle.speed(0)
+turtle.pencolor('green')
+turtle.penup()
+
+turtle.goto(0,200)
+turtle.pendown()
+
+a = 0
+b = 0
+
+while True:
+    turtle.forward(a)
+    turtle.right(b)
+    a = a+3
+    b = b+1
+    if b ==220:
+        time.sleep(3)
+        break
